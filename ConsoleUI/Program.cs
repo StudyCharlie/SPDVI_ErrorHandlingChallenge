4fc27d8584c8f9a2ea84126ef71805aca2ea462a
Carlos <carlosfernandez@paucasesnovescifp.cat>
2020-11-06 17:59:42 +0100

    Tim Corey give us a code with some tipe of Exceptions, using debugging and our brain have to solve it.
    The challenge works with Exceptions of some tipes: IndexOutOfRangeException, FormatExceptions and Exceptions.
    
    Finally added a try catch for manage better the program, seems works fine.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentProcessor paymentProcessor = new PaymentProcessor();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                TransactionModel result = null;
+
+                try
+                {
+                    result = paymentProcessor.MakePayment($"Demo{ i }", i);
+                    
+
+                    if (result != null)
+                    {
+                        Console.WriteLine(result.TransactionAmount);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Null value for item { i }");
+                    }
+                }
+                catch(IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Skip invalid record { i } { ex.InnerException?.Message }");
+                }
+                catch(FormatException ex) when (i != 5)
+                {
+                    Console.WriteLine($"Formating Issue { ex.InnerException?.Message }");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Payment skipped for payment with { i } items { ex.InnerException?.Message }");
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
